be78d579e3264e996f58494cedb25b050af172bf
Anirban Dutta <anirbandutta428@gmail.com>
2022-12-17 21:34:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Dashboard from './Page/Dashboard/Dashboard';
+
+function App() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Page/Dashboard/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header({setIsAdding}) {
+  return (
+    <header>
+        <h1>Employee Management System</h1>
+        <div>
+            <button onClick={()=>{setIsAdding(true)}} className='round-button'>Add Employee</button>
+        </div>
+    </header>
+  )
+}
+
+export default Header
